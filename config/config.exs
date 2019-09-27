2b34165ee1432fbf8358aef61e31a35846aca7bb
Viodine <viodine@yahoo.com>
2019-09-27 19:38:13 +0200

    Configuration for Ecto

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,14 @@
 # move said applications out of the umbrella.
 import Config
 
+config :auction, ecto_repos: [Auction.Repo]
+config :auction, Auction.Repo,
+  database: "auction",
+  username: "viodine",
+  password: "ViGilantE281192!",
+  hostname: "localhost",
+  port: "5432"
+
 config :auction_web,
   generators: [context_app: false]
 
