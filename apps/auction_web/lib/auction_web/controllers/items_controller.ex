19b50e6aa661c763b55d342ef2ec2b16623d1c9b
Viodine <viodine@yahoo.com>
2019-09-28 18:18:29 +0200

    Add new controller for Items related pages

---
+++ apps/auction_web/lib/auction_web/controllers/items_controller.ex
@@ -0,0 +1,8 @@
+defmodule AuctionWeb.ItemsController do
+  use AuctionWeb, :controller
+
+  def index(conn, _params) do
+    items = Auction.list_items()
+    render(conn, "index.html", items: items)
+  end
+end
